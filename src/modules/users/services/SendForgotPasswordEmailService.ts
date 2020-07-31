7cc31b96a9fd9794a75fae729505e69439beaa76
gustavo-bossolani <gustavo.wyd@outlook.com>
2020-07-30 22:48:16 -0300

    :rocket: :wrench: cria teste de envio de email de recuperação de senha

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,32 @@
+import { inject, injectable } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequestDTO {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequestDTO): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
---
+++ src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,6 @@
+// import { container } from 'tsyringe';
+
+// import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+// import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
+// container.registerSingleton<IMailProvider>('MailProvider', FakeMailProvider);
