bb59ba4a17ae538bb15564b5ff34f83562950816
gustavo-bossolani <gustavo.wyd@outlook.com>
2020-08-05 00:44:09 -0300

    :rocket: cria provider de template de email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,12 +6,22 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  // Estamos pedindo para o tsyringe nos fornecer uma instancia de um MailProvider
+  // pois MailProvider provider possui também, uma instância fornecida por injeção de dependencias
+  container.resolve(EtherealMailProvider),
 );
